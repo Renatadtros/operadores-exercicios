b58f3bfa47f722265eb93fbb77cbf2b2aa1de8a1
Renata Detros <renata.detros@gmail.com>
2023-03-16 15:41:45 -0300

    exercicio 1 e 2 finalizado.

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+//Construa um programa que
+//a) Peça ao usuário que insira um número **par**
+//b) Imprima no console **o resto da divisão** desse número por 2.
+//c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+//d) O que acontece no console se o usuário inserir um número ímpar? Escreva em um comentário de código.
+//> **Dica**:   não se esqueça de **converter** as respostas para o tipo número
+//prompt('Insira um número par');
+// ctrl ;   
+
+
+let num1 = Number(prompt('Insira um número par:'));
+console.log("Primeiro numero é:" , num1)
+
+console.log(num1 % 2)
+
+//  Caso o num seja par o resto é sempre 0
+//Caso seja um num ímpar o resto é sempre 1
+
+
+
+
---
+++ exercicio2/script.js
@@ -0,0 +1,20 @@
+//Faça um programa que pergunte ao usuário dois números. Em seguida, faça as operações e imprima no console as seguintes mensagens seguidas pelo `true` ou `false`:
+//O primeiro numero é maior que o segundo? true
+//O primeiro numero é igual ao segundo? false
+//O primeiro numero é divisível pelo segundo? true
+//O segundo numero é divisível pelo primeiro? true
+//divisível = com resto zero
+//Obs: A resposta (true ou false) vai depender dos números inseridos e do resultado das operações. As respostas colocadas acima são apenas exemplos!
+
+let num1 = Number(prompt('Insira um número:'));
+console.log("Primeiro numero é:" , num1);
+
+let num2 =Number(prompt('Insira outro número:'));
+console.log("Segundo número é:" ,num2);
+
+console.log ("O primeiro numero é maior que o segundo?" ,num1 > num2);
+console.log ("Os números são iguais?" ,num1 === num2);
+console.log ("O primeiro número é maior ou igual ao segundo?" ,num1 >= num2);
+
+console.log ("O primeiro número é divisível pelo segundo? " ,(num1 % num2) === 0);
+console.log ("O segundo número é divisível pelo primeiro?"  ,(num2 % num1) === 0);
